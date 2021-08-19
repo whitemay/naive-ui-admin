6c05ffb12439800e4c8818cf049eb483ea38693a
aston <356386@qq.com>
2021-08-19 09:20:00 +0800

    简化AppProvider

--- src/components/Application/index.ts
+++ src/components/Application/index.ts
@@ -1,3 +1,63 @@
-import AppProvider from './Application.vue';
+import { defineComponent } from 'vue';
+import { useMessage, useDialog, useLoadingBar } from 'naive-ui';
+import { loadingSubject, messageSubject, modalSubject } from '@/utils/subjects';
 
-export { AppProvider };
+export const AppProvider = defineComponent({
+  name: 'Application',
+  setup() {
+    const loadingBar = useLoadingBar();
+    const message = useMessage();
+    const dialog = useDialog();
+
+    loadingSubject.subscribe({
+      next: (status) => {
+        if (status == 'start') {
+          loadingBar.start();
+        } else if (status == 'error') {
+          loadingBar.error();
+        } else {
+          loadingBar.finish();
+        }
+      },
+    });
+
+    messageSubject.subscribe((event: any) => {
+      switch (event.type) {
+        case 'error': {
+          message.error(event.info);
+          break;
+        }
+        case 'warning': {
+          message.warning(event.info);
+          break;
+        }
+        case 'info': {
+          message.info(event.info);
+          break;
+        }
+        case 'success': {
+          message.success(event.info);
+        }
+      }
+    });
+
+    modalSubject.subscribe((event: any) => {
+      switch (event.type) {
+        case 'info': {
+          dialog.info(event.params);
+          break;
+        }
+        case 'warning': {
+          dialog.warning(event.params);
+          break;
+        }
+      }
+    });
+
+    return {};
+  },
+  render() {
+    function noop() {} // 单纯为了避免TS报错
+    return [(this.$slots.default || noop)()];
+  },
+});
